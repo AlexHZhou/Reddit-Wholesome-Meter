b6b4e37c78af8ca33bf443edc6d7dd0a2557df3c
Alex Zhou <alexxzhou99@gmail.com>
2019-10-12 17:05:24 -0700

    test git

--- testing.py
+++ testing.py
@@ -1,3 +1,22 @@
 import requests
+from bs4 import BeautifulSoup
 
-print('fuck');
+
+response = requests.get('https://www.reddit.com/r/wholesomememes/comments/dgz1n8/wholesome_hotel_horn/')
+
+continueSpam = True
+while continueSpam:
+    if response:
+        print('Success accessing server!')
+        continueSpam = False
+    else:
+        print('Problem accessing server.', response.status_code)
+
+    response = requests.get('https://www.reddit.com/r/wholesomememes/comments/dgz1n8/wholesome_hotel_horn/')
+# print(response.text)
+# print(response.json())
+# print(response.headers);
+
+soup = BeautifulSoup(response.text, 'html.parser')
+soup.prettify()
+print(soup.p)
