f4037b6bdecbbfc8dbb460eeb2928eb9445fdb2c
Alex Zhou <alexxzhou99@gmail.com>
2019-10-12 21:38:57 -0700

    testing fixed gitignore

--- testing.py
+++ testing.py
@@ -1,20 +1,81 @@
-import requests
-from bs4 import BeautifulSoup
+import numpy
+import praw
+
+from textblob import TextBlob
+
+import nltk
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+#!pip install flair
+# import flair
+# flair_sentiment = flair.models.TextClassifier.load('en-sentiment')
 
 
-response = requests.get('https://www.reddit.com/r/wholesomememes/comments/dgz1n8/wholesome_hotel_horn/')
 
-continueSpam = True
-while continueSpam:
-    if response:
-        print('Success accessing server!')
-        continueSpam = False
-    else:
-        print('Problem accessing server.', response.status_code)
+reddit = praw.Reddit(client_id='gPkxqeRaHjXmug', \
+                     client_secret='xNCTXJQQQIIsIGG3M9UF_DOX2I8', \
+                     user_agent='dubhackScrape', \
+                     username='scrollAway_throwAway', \
+                     password='dubHacks2019')
 
-    response = requests.get('https://www.reddit.com/r/wholesomememes/comments/dgz1n8/wholesome_hotel_horn/')
+subreddits = ['WholesomeMemes', 'meirl', 'politics', 'math', 'learnpython', 'funny', 'gaming', 'Music', 'travel']
+for x in subreddits:
+    subreddit = reddit.subreddit(x)
 
+    sid = SentimentIntensityAnalyzer()
 
-soup = BeautifulSoup(response.text, 'html.parser')
-soup.prettify()
-print(soup.p)
+    avgSubmissionNeg = []
+    avgSubmissionPos = []
+    avgSubmissionNeu = []
+    avgSubmissionPolar = []
+    avgSubmissionSubj = []
+    for submission in subreddit.hot(limit=8):
+        negative = 0
+        positive = 0
+        neutral = 0
+        polar = 0
+        subjective = 0
+
+        numComments = 0
+        for comment in submission.comments:
+            try:
+                polarity = sid.polarity_scores(comment.body)
+                opinion = TextBlob(comment.body)
+                negative += polarity.get('neg')
+                neutral += polarity.get('neu')
+                positive += polarity.get('pos')
+
+                polar += opinion.polarity
+                subjective += opinion.sentiment[1]
+
+                numComments += 1
+            except:
+                continue;
+        if numComments == 0: continue
+        negative /= numComments
+        neutral /= numComments
+        positive /= numComments
+        polar /= numComments
+        subjective /= numComments
+        avgSubmissionNeg.append(negative)
+        avgSubmissionNeu.append(neutral)
+        avgSubmissionPos.append(positive)
+        avgSubmissionPolar.append(polar)
+        avgSubmissionSubj.append(subjective)
+
+    subRedditAverage = [0, 0, 0, 0, 0]
+    length = len(avgSubmissionNeg)
+    for n in range(length):
+        subRedditAverage[0] += avgSubmissionNeg[n]
+        subRedditAverage[1] += avgSubmissionNeu[n]
+        subRedditAverage[2] += avgSubmissionPos[n]
+        subRedditAverage[3] += avgSubmissionPolar[n]
+        subRedditAverage[4] += avgSubmissionSubj[n]
+
+    print("Info about r/", x)
+    print("negativity: ", round(subRedditAverage[0] / length, 3))
+    print("neutral: ", round(subRedditAverage[1] / length,3))
+    print("positivity: ", round(subRedditAverage[2] / length,3))
+    print("Polarization: ", round(subRedditAverage[3] / length, 3))
+    print("Subjectivity: ", round(subRedditAverage[4] / length, 3), '\n')
