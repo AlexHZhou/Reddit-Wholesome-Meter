9deb992423a1090334e56cdc1fcbee333b616d18
ryanMccray <ryan_mccray@utexas.edu>
2019-10-12 20:51:32 -0500

    pretrained model added
    added flair pretrained model, nltk sentiment analyzer

--- testing2.py
+++ testing2.py
@@ -1,6 +1,16 @@
 import numpy
 import praw
+
 from textblob import TextBlob
+
+import nltk
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+#!pip install flair
+import flair
+flair_sentiment = flair.models.TextClassifier.load('en-sentiment')
+
 
 
 reddit = praw.Reddit(client_id='gPkxqeRaHjXmug', \
@@ -11,8 +21,16 @@ reddit = praw.Reddit(client_id='gPkxqeRaHjXmug', \
 
 subreddit = reddit.subreddit('WholesomeMemes')
 
-for submission in subreddit.top(limit=1):
+sid = SentimentIntensityAnalyzer()
+
+for submission in subreddit.hot(limit=1):
     for comment in submission.comments:
         opinion = TextBlob(comment.body)
+        s = flair.data.Sentence(comment.body)
+        flair_sentiment.predict(s)
+        total_sentiment = s.labels
+        print(total_sentiment)
+        print(comment.body)
+        print(sid.polarity_scores(comment.body))
         print(opinion.sentiment)
         print("\n")
